74b8ee43b3bab97bf353c61ffb39a01fb3196e8c
Maykmora <maykmora12@gmail.com>
2025-07-31 08:49:28 -0600

    Commit 2

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -6,3 +6,10 @@ for i in range(p):
     name=input("Ingrese el nombre del propietario: ")
     phone=int(input("Ingrese el numero de teléfono del propietario:"))
     cantidad=int(input("Ingrese la cantidad de vehículos que posee: "))
+    for j in range(cantidad):
+        print("\nVehículo No.1:")
+        placa=input("Ingrese el número de placa del vehículo: ")
+        marca=input("Ingrese la marca del vehiculo: ")
+        year=int(input("Ingrese el modelo del vehículo: "))
+        estado=input("Ingrese el estado del impuesto (sí-no): ")
+
