5e02ff3741e57b34204ec606da8528fd27b02a86
Maykmora <maykmora12@gmail.com>
2025-07-31 09:36:53 -0600

    Commit 5

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -9,27 +9,41 @@ for i in range(p):
     phone=int(input("Ingrese el numero de teléfono del propietario:"))
     cantidad=int(input("Ingrese la cantidad de vehículos que posee: "))
     for j in range(cantidad):
-        print("\nVehículo No.1:")
+        print(f"\nVehículo No.{j+1}:")
         placa=input("Ingrese el número de placa del vehículo: ")
         marca=input("Ingrese la marca del vehiculo: ")
-        year=int(input("Ingrese el modelo del vehículo: "))
+        modelo=input("Ingrese el modelo del carro:")
+        year=int(input("Ingrese el año del vehículo: "))
         estado=input("Ingrese el estado del impuesto (sí-no): ")
-        info_vehiculo["placa"]={
+        info_vehiculo[placa]={
             "marca":marca,
-            "modelo":year,
+            "modelo":modelo,
+            "año":year,
             "estado":estado
         }
 
-    propietarios["nit"]={
+    propietarios[nit]={
         "nombre":name,
         "teléfono":phone,
-        "Vehículos":[
-            {
-                "placa":placa,
-                "marca":marca,
-                "modelo":year,
-                "impuesto_pagado":estado
-        }],
+        "Vehículos":[info_vehiculo]
     }
+print("\n--LISTADO DE PRODUCTOS--")
+contador1=1
+for nit,datos in propietarios.items():
+    contador2 = 1
+    print(f"Propietario No.{contador1}")
+    print(f"NIT: {nit}")
+    print(f"Nombre:{datos["nombre"]}")
+    print(f"Teléfono:{datos["teléfono"]}")
+    for placa,infor in info_vehiculo.items():
+        print(f"\nVehículo No.{contador2}")
+        print(f"Placa: {placa}")
+        print(f"Marca: {infor["marca"]}")
+        print(f"Modelo:{infor["modelo"]}")
+        print(f"Año:{infor["año"]}")
+        print(f"Estado:{infor["estado"]}")
+        contador2+=1
 
-print(propietarios)
+
+
+
